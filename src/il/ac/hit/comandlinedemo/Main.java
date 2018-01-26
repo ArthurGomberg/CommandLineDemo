0d6c9c6dd3fd129c49bc9413850f6b06bbf579bc
Arthur Gomberg <gombergarthur@gmail.com>
2018-01-26 21:40:00 +0200

    New Person class Added to Project

--- src/il/ac/hit/comandlinedemo/Main.java
+++ src/il/ac/hit/comandlinedemo/Main.java
@@ -3,7 +3,19 @@ package il.ac.hit.comandlinedemo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("One more change to commit...");
+        System.out.println("Here new code comes ...");
+        Person firstPerson = new Person("George", "Smith", 199);
+        Person secondPerson = new Person("Sam", "Jones", 2005);
+        System.out.println("Get person's details :");
+
+        System.out.println("First Person : " + firstPerson.getFirstName() + " " +
+                firstPerson.getLastName() + " " + firstPerson.getId());
+
+        System.out.println("Second Person : " + secondPerson.getFirstName() + " " +
+                secondPerson.getLastName() + " " + secondPerson.getId());
+
+
     }
 }
